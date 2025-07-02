d5709ef93898cf1e0909a76f35987b6fc8165652
adamcaak <ladamczak1997@gmail.com>
2025-07-02 23:24:01 +0200

    Initial Commit

---
+++ ColorGrid/ColorGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGridApp.swift
+//  ColorGrid
+//
+//  Created by Łukasz Adamczak on 02/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
