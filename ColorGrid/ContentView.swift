e98c2e4185b76bcf8d4b18dddd782e603010ba60
adamcaak <ladamczak1997@gmail.com>
2025-07-02 23:37:54 +0200

    Create grid

--- ColorGrid/ContentView.swift
+++ ColorGrid/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columnsLayout = Array(repeating: GridItem(), count: 3)
+    @State private var selectedColor = Color.gray
+    let allColors: [Color] = [
+        .pink,
+        .red,
+        .orange,
+        .yellow,
+        .green,
+        .mint,
+        .teal,
+        .cyan,
+        .blue,
+        .indigo,
+        .purple,
+        .brown,
+        .gray,
+        .black
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Selected Color")
+                .font(.body)
+                .fontWeight(.semibold)
+                .foregroundColor(selectedColor)
+                .padding(10)
+            
+            ScrollView {
+                LazyVGrid(columns: columnsLayout) {
+                    ForEach(allColors, id: \.description) { color in
+                        Button {
+                            selectedColor = color
+                        } label: {
+                            RoundedRectangle(cornerRadius: 4.0)
+                                .aspectRatio(1.0, contentMode: ContentMode.fit)
+                                .foregroundColor(color)
+                        }
+                        .buttonStyle(.plain)
+                    }
+                }
+            }
         }
-        .padding()
+        .padding(10)
     }
 }
 
